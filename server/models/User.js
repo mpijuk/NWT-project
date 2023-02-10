c5ec910358bbe5127392343a55c45cef83069ac5
Mario Pijuk <mpijuk00@fesb.hr>
2023-02-10 23:46:03 +0100

    User and Post Routes Setup

--- server/models/User.js
+++ server/models/User.js
@@ -29,7 +29,7 @@ const UserSchema = new mongoose.Schema(
             type: String,
             default: "",
         },
-        friend: {
+        friends: {
             type: Array,
             default: [],
         },
